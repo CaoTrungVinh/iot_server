603f5918ac4e2b805fbebf851ed0b099f8756ce8
CaoTrungVinh <caotrungvinh199@gmail.com>
2021-06-11 15:43:44 +0700

    register and loginAdmin

--- iot/app/Http/Kernel.php
+++ iot/app/Http/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\CheckIsUser;
+use App\Http\Middleware\CheckLogin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -62,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'checkisuser'                        => CheckIsUser::class,
+        'checklogin'                            => CheckLogin::class,
+        'checkisadmin'                              => \App\Http\Middleware\CheckIsAdmin::class,
     ];
 }
--- iot/routes/web.php
+++ iot/routes/web.php
@@ -4,24 +4,32 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DHT11Controller;
 
 
-Route::get('/', function () {
-    return view('pages.home');
-})->name('home');
+//Route::get('/', function () {
+//    return view('pages.home');
+//})->name('home');
 //Route::get('/dht11', function () {
 //    return view('pages.dht11');
 //})->name('dht11');
+
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('register', [\App\Http\Controllers\Auth\RegisterController::class, 'register'])->name('register');
+Route::post( 'register', [\App\Http\Controllers\Auth\RegisterController::class, 'doRegister'])->name( 'register' );
+Route::get('register', [\App\Http\Controllers\Auth\RegisterController::class, 'showRegister'])->name('register');
+Route::get( 'confirmemail/{email}/{key}', [\App\Http\Controllers\Auth\RegisterController::class, 'confirmEmail'] )->name( 'confirmemail' );
+
+//  route admin
+Route::get('login', [\App\Http\Controllers\Admin\AdminLoginController::class, 'showLogin'])->name('login');
+Route::post('postAdminLogin', [\App\Http\Controllers\Admin\AdminLoginController::class, 'postLogin'])->name('postAdLogin');
+Route::get('getAdminLogout', [\App\Http\Controllers\Admin\AdminLoginController::class, 'getLogout'])->name('getAdLogout');
 
 //Route::get('/DHT11', 'DHT11Controller@index');
 Route::get('/DHT11', [\App\Http\Controllers\Admin\DHT11Controller::class, 'index']);
 Route::get('/DHT11/{id}', 'DHT11Controller@view');
 
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
 Route::get('/forgot_password', function () {
     return view('auth.forgot-password');
 })->name('forgot_password');
