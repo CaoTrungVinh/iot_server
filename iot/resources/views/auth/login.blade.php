097d57686a063aca2abfb66e414a3f70f6790649
ChuongNguyen1123 <nguyenckuong98@gmail.com>
2021-06-21 14:26:00 +0700

    register, adminlogin

--- iot/resources/views/auth/login.blade.php
+++ iot/resources/views/auth/login.blade.php
@@ -1,40 +1,59 @@
 @extends('auth.auth')
 @section('content-auth')
 
-<div class="login-box">
-    <h1 class="text-center mb-5"><i class="fa fa-rocket text-primary"></i> Sleekadmin</h1>
-    <div class="row">
-        <div class="col-md-6 col-sm-6 col-12 login-box-info">
-            <h3 class="mb-4">Sign up</h3>
-            <p class="mb-4">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
-            <p class="text-center"><a href="{{route('register')}}" class="btn btn-light">Register here</a></p>
-        </div>
-        <div class="col-md-6 col-sm-6 col-12 login-box-form p-4">
-            <h3 class="mb-2">Login</h3>
-            <small class="text-muted bc-description">Sign in with your credentials</small>
-            <form action="" class="mt-2">
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-user"></i></span>
-                    </div>
-                    <input type="text" class="form-control mt-0" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-                </div>
+    <div class="login-box">
+        <h1 class="text-center mb-5"><i class="fa fa-rocket text-primary"></i> Sleekadmin</h1>
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-12 login-box-info">
+                <h3 class="mb-4">Sign up</h3>
+                <p class="mb-4">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
+                <p class="text-center"><a href="{{route('register')}}" class="btn btn-light">Register here</a></p>
+            </div>
+            <div class="col-md-6 col-sm-6 col-12 login-box-form p-4">
+                <h3 class="mb-2">Login</h3>
+                <small class="text-muted bc-description">Sign in with your credentials</small>
 
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-lock"></i></span>
-                    </div>
-                    <input type="text" class="form-control mt-0" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
-                </div>
+                <form method="post" action="{{route('postAdLogin')}}" class="mt-2">
+                    @csrf
+                    @error('mes')
+                    <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
 
-                <div class="form-group">
-                    <button class="btn btn-theme btn-block p-2 mb-1">Login</button>
-                    <a href="{{route('forgot_password')}}">
-                        <small class="text-theme"><strong>Forgot password?</strong></small>
-                    </a>
-                </div>
-            </form>
+                    @if(Session::has('ok'))
+                        <small class="form-text text-success">{{ Session::get('ok') }}</small>
+                    @endif
+                    <p class="form">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fa fa-user"></i></span>
+                        </div>
+                        <input type="text" class="form-control mt-0" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1" name="email" id="email" value="{{ old('email') }}" />
+                    </div>
+                    @error('email')
+                    <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                    </p>
+
+                    <p class="form">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fa fa-lock"></i></span>
+                        </div>
+                        <input type="password" class="form-control mt-0" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" name="pass" id="pass" />
+                    </div>
+                    @error('pass')
+                    <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                    </p>
+
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-theme btn-block p-2 mb-1" name="login" value="Login" />
+                        <a href="{{route('forgot_password')}}">
+                            <small class="text-theme"><strong>Forgot password?</strong></small>
+                        </a>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
-@endsection
+@endsection()
--- iot/resources/views/auth/register.blade.php
+++ iot/resources/views/auth/register.blade.php
@@ -1,51 +1,84 @@
 @extends('auth.auth')
 @section('content-auth')
     <div class="login-box">
-    <h1 class="text-center mb-5"><i class="fa fa-rocket text-primary"></i> Sleekadmin</h1>
-    <div class="row">
-        <div class="col-md-6 col-sm-6 col-12 login-box-info">
-            <h3 class="mb-4">Sign in</h3>
-            <p class="mb-4">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
-            <p class="text-center"><a href="{{route('login')}}" class="btn btn-light">Login here</a></p>
-        </div>
-        <div class="col-md-6 col-sm-6 col-12 login-box-form p-4">
-            <h3 class="mb-2">Sign up</h3>
-            <small class="text-muted bc-description">Create new account</small>
-            <form action="" class="mt-2">
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-user"></i></span>
-                    </div>
-                    <input type="text" class="form-control mt-0" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-                </div>
+        <h1 class="text-center mb-5"><i class="fa fa-rocket text-primary"></i> Sleekadmin</h1>
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-12 login-box-info">
+                <h3 class="mb-4">Sign in</h3>
+                <p class="mb-4">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.</p>
+                <p class="text-center"><a href="{{route('login')}}" class="btn btn-light">Login here</a></p>
+            </div>
+            <div class="col-md-6 col-sm-6 col-12 login-box-form p-4">
+                <h3 class="mb-2">Sign up</h3>
+                <small class="text-muted bc-description">Create new account</small>
 
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-envelope"></i></span>
+                {{--          form Register         --}}
+                <form method="post" action="{{route('register')}}" class="register">
+                    @csrf
+                    <p class="form">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fa fa-user"></i></span>
+                        </div>
+                        <input type="text" class="form-control mt-0" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" name="r_userName" id="r_userName" value="{{old('r_firstname')}}" />
                     </div>
-                    <input type="email" class="form-control mt-0" placeholder="johndoe@gmail.com" aria-label="email" aria-describedby="basic-addon1">
-                </div>
-
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-phone"></i></span>
+                    @error('r_userName')
+                    <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                    </p>
+                    <p class="form">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fa fa-envelope"></i></span>
+                        </div>
+                        <input type="email" class="form-control mt-0" placeholder="Email" aria-label="email" aria-describedby="basic-addon1" name="r_email" id="r_email" value="{{old('r_email')}}" />
                     </div>
-                    <input type="text" class="form-control mt-0" placeholder="555-098-444" aria-label="phone" aria-describedby="basic-addon1">
-                </div>
-
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="basic-addon1"><i class="fa fa-lock"></i></span>
+                    @error('r_email')
+                    <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                    </p>
+                    <p class="form">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fa fa-lock"></i></span>
+                        </div>
+                        <input type="password" class="form-control mt-0" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1" name="r_pass" id="r_pass" />
                     </div>
-                    <input type="text" class="form-control mt-0" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
-                </div>
+                    @error('r_pass')
+                    <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                    </p>
+                    <p class="form">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="fa fa-lock"></i></span>
+                        </div>
+                        <input type="password" class="form-control mt-0" placeholder="Repeat Password" aria-label="Repeat Password" aria-describedby="basic-addon1" name="r_repass" id="r_repass" />
+                    </div>
+                    @error('r_repass')
+                    <small class="form-text text-danger">{{ $message }}</small>
+                    @enderror
+                    </p>
 
-                <div class="form-group">
-                    <button class="btn btn-theme btn-block p-2 mb-1">Register</button>
-                </div>
-            </form>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-theme btn-block p-2 mb-1" name="register" value="Register" />
+                        {{--                    <button class="btn btn-theme btn-block p-2 mb-1">Register</button>--}}
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
 @endsection
+
+<!-- Script to active register
+    ================================================== -->
+@section('lastScript')
+    @if ($register ?? ''!='')
+        <script>
+            $( document ).ready(function() {
+                $('.tabs-nav a[href$="{{$register}}"]').parent("li").click();
+            });
+        </script>
+    @endif
+@endsection()
